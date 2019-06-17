781f6c130415c8a59f12c6d7a7e5440d2e221f7e
Jurgen.Porubenger <jurgenporubenger@gmail.com>
2019-06-17 18:06:30 +0300

    last

--- script.js
+++ script.js
@@ -1,51 +1,3 @@
-//
-// let point = document.querySelector('#point');
-// let main = document.querySelector('#main');
-// let body = document.querySelector('body');
-//
-// point.addEventListener("mousedown", baba);
-// function baba () {
-//     main.addEventListener("mousemove", dada );
-// }
-// function dada () {
-//     let box = main.getBoundingClientRect();
-//     // console.log(box);
-//     // let centerX = main.documentElement.clientWidth;
-//     let X = event.clientX;
-//     let Y = event.clientY;
-//     if (Y>box.top+point.offsetWidth/2&&Y<box.bottom-point.offsetWidth/2
-//         &&X>box.left+point.offsetWidth/2&&X<box.right-point.offsetWidth/2) {
-//
-//         point.style.left = (X-point.offsetWidth/2-box.left) + 'px';
-//         point.style.top = (Y-point.offsetWidth/2-box.top) + 'px';
-//     }
-// }
-//
-// point.addEventListener("mouseup", function () {
-//     main.removeEventListener("mousemove" ,dada);
-// });
-// body.addEventListener("mouseup", function () {
-//     main.removeEventListener("mousemove" ,dada);
-// });
-
-//
-// function f(func) {
-//
-//     console.log(func());
-// }
-// f(f1);
-
-
-// function f2(a) {
-//     console.log(a);
-// }
-// function f1(func, val) {
-//     setTimeout(function () {
-//         func(val);
-//     }, 1000);
-// }
-// f1(f2, "5");
-
 
 
 // function f(t) {
@@ -58,29 +10,89 @@
 // }
 // f1(f,"ere");
 
+//===========================================
 
-
-
-
-
-
-
-
-
-
-setTimeout(function(){
-    greet(randomGreet);
-}, 1000);
-function greet(greeting){
-    console.log(greeting);
-}
-
+//
+// setTimeout(function(){
+//     greet(randomGreet);
+// }, 1000);
+//
+// function greet(greeting){
+//     console.log(greeting);
+// }
+//
 // let greetings = ["Hello", "Bonjour", "Guten Tag"],
-    randomGreet = getRandom(greetings);
+//
+//     randomGreet = getRandom(greetings);
+//
+// function getRandom(arr){
+//     return arr[Math.floor(Math.random()*arr.length)];
+// }
 
-function getRandom(arr){
-    return arr[Math.floor(Math.random()*arr.length)];
+
+//============================================================
+
+
+let point = document.querySelector('#point');
+let main = document.querySelector('#main');
+let body = document.querySelector('body');
+
+point.addEventListener("mousedown", baba);
+function baba () {
+    main.addEventListener("mousemove", dada );
 }
+function dada () {
+    let box = main.getBoundingClientRect();
+    // console.log(box);
+    // let centerX = main.documentElement.clientWidth;
+    let X = event.clientX;
+    let Y = event.clientY;
+    if (Y>box.top+point.offsetWidth/2&&Y<box.bottom-point.offsetWidth/2
+        &&X>box.left+point.offsetWidth/2&&X<box.right-point.offsetWidth/2) {
+
+        point.style.left = (X-point.offsetWidth/2-box.left) + 'px';
+        point.style.top = (Y-point.offsetWidth/2-box.top) + 'px';
+    }
+}
+
+point.addEventListener("mouseup", function () {
+    main.removeEventListener("mousemove" ,dada);
+});
+body.addEventListener("mouseup", function () {
+    main.removeEventListener("mousemove" ,dada);
+});
+
+
+
+
+
+//================================================================
+
+
+
+
+function randNumb(num){
+    return Math.floor(num*Math.random());
+}
+
+function f () {
+   setTimeout(function(){
+       console.log(randNumb(10));
+    }, 1000);
+}
+
+function f1() {
+    let x = f;
+    x();
+
+}
+f1();
+
+
+
+
+
+
 
 
 
